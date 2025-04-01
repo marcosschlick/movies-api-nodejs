70185fe300e59fdd3ceb0f7a6ccb6b0b3c3fe3b0
Marcos Schlick <marcosschlick@gmail.com>
2025-04-01 12:06:51 -0300

    atualizar métodos da base de dados em memória

--- database-memory.js
+++ database-memory.js
@@ -6,13 +6,29 @@ export class DatabaseMemory {
     // Set -> parecido com array mas não aceita valores duplicados
     // Map -> estrutura parecida com objetos no js pela chave e valor, mas tem métodos diferentes que um objeto 
 
-    list() {
-        this.#videos.values()
+    list(search) {
+        return Array.from(this.#videos.entries())
+            .map((videoArray) => {
+                const id = videoArray[0]
+                const data = videoArray[1]
+                
+                return {
+                    id,
+                    ...data
+                }
+        })
+        .filter(video => {
+            if (search) {
+                return video.title.includes(search)
+            }
+
+            return true
+        })
     }
 
     create(video) {
         // UUID -> Universally Unique Identifier
-        const videoId = randomUUID
+        const videoId = randomUUID()
 
         this.#videos.set(videoId, video)
     }
