b88cbd284f86aa67776810e873f2f4d6b03192ba
Marcos Schlick <marcosschlick@gmail.com>
2025-04-01 12:07:28 -0300

    adicionar rotas GET, POST, PUT e DELETE para a API

--- server.js
+++ server.js
@@ -11,19 +11,61 @@ server.listen(3333)
 */
 
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
 
-server.get('/', () => {
-    return 'Hello, World!'
+const database = new DatabaseMemory()
+
+// GET -> buscar um ou vários registros
+// POST -> ciar um registro
+// PUT -> alterar um registro
+// DELLETE -> deletar um registro
+
+// Route Parameter -> parâmetro dentro da rota `:id`
+// Request Body -> corpo da requisição usado para `Post` e `Put`
+// Query ou SearcH Params -> filtrar dados, normalmente usando em rotas de busca,
+
+server.post('/videos', (request, reply) => {
+    const { title, description, duration } = request.body
+    database.create({
+        // title: title,
+        // description: description,
+        // duration: duration
+        title,
+        description,
+        duration
+    })
+    // 201 ->  algo foi criado
+    return reply.status(201).send()
 })
 
-server.get('/ptbr', () => {
-    return 'Olá, Mundo!'
+server.get('/videos', (request) => {
+    const search = request.query.search
+    const videos = database.list(search)
+    return videos
 })
 
-server.get('/esuy', () => {
-    return 'Hola, Mundo!'
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id 
+    const { title, description, duration } = request.body
+
+    const video = database.update(videoId, {
+        title,
+        description,
+        duration
+    })
+
+    // 204 -> a respota teve sucesso mas o corpo é vazio
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id 
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
