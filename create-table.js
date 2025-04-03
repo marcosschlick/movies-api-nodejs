daf6aa9618e671b263177793ab010ca33d7b18a0
Marcos Schlick <marcosschlick@gmail.com>
2025-04-03 13:35:11 -0300

    adicionar a propriedade `id` na tabela movies

--- create-table.js
+++ create-table.js
@@ -1,7 +1,12 @@
 import sql from './db.js'
 
+sql`DROP TABLE IF EXISTS videos;`.then(() => {
+    console.log('Tabela excluída com sucesso!')
+})
+
 sql`
     CREATE TABLE videos (
+        id TEXT PRIMARY KEY,
         title TEXT,
         description TEXT,
         duration INTEGER
